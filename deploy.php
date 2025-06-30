c8261e01462887338290e8224463e6c8d983ddc3
kasmiamjad <kasmi.amjad@gmail.com>
2025-06-30 15:32:52 +0530

    new login update4

--- deploy.php
+++ deploy.php
@@ -1,6 +1,9 @@
 <?php
 
-    file_put_contents('deploy.log', date('c')." Deployment triggered\n", FILE_APPEND);
-    shell_exec('cd /home/yszraxwq/mumbrahillresort.com/calender && git pull origin main');
-    echo "Deployed!";
+   file_put_contents('deploy.log', date('c')." Deployment triggered\\n", FILE_APPEND);
+$output = shell_exec('cd /home/yszraxwq/mumbrahillresort.com/calender && git pull origin main');
+file_put_contents('deploy.log', $output, FILE_APPEND);
+file_put_contents('deploy.log', "Deployed!\\n", FILE_APPEND);
+echo "Deployed!";
+
 ?>
