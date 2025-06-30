418ad4a709d1939452fdd7239b61a66d99ce7629
kasmiamjad <kasmi.amjad@gmail.com>
2025-06-30 15:20:09 +0530

    new login update3

--- login.php
+++ login.php
@@ -268,7 +268,7 @@ $pending_amount = $total_due - $total_paid;
 <body>
     <div class="app-container">
         <div class="login-header">
-            <h1>Mumbra Hill Resort Booking System 2</h1>
+            <h1>Mumbra Hill Resort Booking System</h1>
             <p>Welcome back! Please login to continue</p>
         </div>
 
