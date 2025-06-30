301fc22242a0e208c2e0b19f27a7bf9fff946389
kasmiamjad <kasmi.amjad@gmail.com>
2025-06-30 14:57:42 +0530

    add log to deploy file

--- deploy.php
+++ deploy.php
@@ -1,4 +1,6 @@
 <?php
-  shell_exec('cd /home/yszraxwq/mumbrahillresort.com/calender && git pull origin main');
-  echo "Deployed!";
+
+    file_put_contents('deploy.log', date('c')." Deployment triggered\n", FILE_APPEND);
+    shell_exec('cd /home/yszraxwq/mumbrahillresort.com/calender && git pull origin main');
+    echo "Deployed!";
 ?>
